17a7ae013f5a1b7a0a1cd00942c793e211fd5b24
Federico Allegrini <federico.allegrini.dev@gmail.com>
2022-03-26 11:13:35 +0100

    Programmatically changing the page after product creation

--- frontend/components/CreateProduct.js
+++ frontend/components/CreateProduct.js
@@ -1,5 +1,6 @@
 import gql from 'graphql-tag';
 import { useMutation } from '@apollo/client';
+import Router from 'next/router';
 import useForm from '../lib/useForm';
 import Form from './styles/Form';
 import DisplayError from './ErrorMessage';
@@ -49,8 +50,12 @@ export default function CreateProduct() {
       onSubmit={async (e) => {
         e.preventDefault();
         // Submit the input fields to the backend
-        await createProduct();
+        const res = await createProduct();
         clearForm();
+        // Go to that product's page!
+        Router.push({
+          pathname: `/product/${res.data.createProduct.id}`,
+        });
       }}
     >
       <DisplayError error={error} />
