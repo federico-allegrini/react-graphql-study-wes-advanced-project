9070e8b00cad363497bda704dadf6b01092f2856
folkdev <folk.development@gmail.com>
2022-04-06 12:03:56 +0200

    Cart - Remove from cart button

---
+++ frontend/components/RemoveFromCart.js
@@ -0,0 +1,37 @@
+import { useMutation } from '@apollo/client';
+import gql from 'graphql-tag';
+import styled from 'styled-components';
+
+const BigButton = styled.button`
+  font-size: 3rem;
+  background: none;
+  border: 0;
+  &:hover {
+    color: var(--red);
+    cursor: pointer;
+  }
+`;
+
+const REMOVE_FROM_CART = gql`
+  mutation REMOVE_FROM_CART($id: ID!) {
+    deleteCartItem(id: $id) {
+      id
+    }
+  }
+`;
+
+export default function RemoveFromCart({ id }) {
+  const [deleteCartItem, { loading }] = useMutation(REMOVE_FROM_CART, {
+    variables: { id },
+  });
+  return (
+    <BigButton
+      onClick={deleteCartItem}
+      disabled={loading}
+      type="button"
+      title="Remove This Item from Cart"
+    >
+      &times;
+    </BigButton>
+  );
+}
