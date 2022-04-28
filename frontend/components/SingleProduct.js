464066870adf09ee4d8c65be4bd0784d3e8c7dff
Federico Allegrini <federico.allegrini.dev@gmail.com>
2022-04-28 12:55:17 +0200

    Mocking graphql data requests

--- frontend/components/SingleProduct.js
+++ frontend/components/SingleProduct.js
@@ -45,7 +45,7 @@ export default function SingleProduct({ id }) {
   if (error) return <DisplayError error={error} />;
   const { Product } = data;
   return (
-    <ProductStyles>
+    <ProductStyles data-testid="singleProduct">
       <Head>
         <title>Sick Fits | {Product.name}</title>
       </Head>
@@ -60,3 +60,5 @@ export default function SingleProduct({ id }) {
     </ProductStyles>
   );
 }
+
+export { SINGLE_ITEM_QUERY };
